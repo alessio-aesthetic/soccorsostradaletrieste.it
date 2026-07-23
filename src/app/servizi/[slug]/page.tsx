fe69a4f00a8f2b93043eda391ec14f7153e4eb31
alessio-aesthetic <alessi.ola.rocchia@gmail.com>
2026-07-23 19:06:20 +0200

    Create Trieste roadside assistance site

--- src/app/servizi/[slug]/page.tsx
+++ src/app/servizi/[slug]/page.tsx
@@ -133,3 +133,4 @@ export default function ServicePage({ params }: { params: { slug: string } }) {
   )
 }
 
+
--- src/app/zone/[slug]/page.tsx
+++ src/app/zone/[slug]/page.tsx
@@ -124,3 +124,4 @@ export default function ZonePage({ params }: { params: { slug: string } }) {
   )
 }
 
+
